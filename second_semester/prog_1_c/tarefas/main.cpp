f3a7a0e96ba20d44a2bb9d900a5efac24748a1e5
aquides <arquimdesaquides@gmail.com>
2022-09-01 18:27:22 -0300

    test

---
+++ second_semester/prog_1_c/tarefas/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    /*1 - Escreva um programa que leia um valor inteiro pelo teclado e imprima a mensagem:
+    Valor lido: n onde n é o valor lido pelo programa.*/
+   
+    //TYPE
+    int x;
+    
+    //CODE
+    printf("Digite o valor de x ");
+        scanf( "%d", &x );
+    printf( "O valor de x é %d\n: ", x );
