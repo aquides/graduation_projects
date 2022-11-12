07c492f287dee788293fd7290bc2496220831d44
aquides <arquimedesaquides@gmail.com>
2022-11-12 17:12:36 -0300

    aula_9_prog_pitao

---
+++ second_semester/prog_1_c/main.c
@@ -0,0 +1,4 @@
+Escreva um programa que leia um inteiro N e crie uma matriz alocada
+dinamicamente contendo N linhas e N colunas. Essa matriz deve conter o valor 0 na
+diagonal principal, o valor 1 nos elementos acima da diagonal principal e o valor −1
+nos elementos abaixo da diagonal principal.
